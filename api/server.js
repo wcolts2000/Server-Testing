0862c1d947fcfa935206680f73726f87743c5c1e
wcolts2000 <wcolts2000@gmail.com>
2019-01-31 13:29:19 -0800

    added post to users route and testing status code and responce object being as epected

--- api/server.js
+++ api/server.js
@@ -10,4 +10,10 @@ server.get("/", (req, res) => {
   res.json("good to go");
 });
 
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+
+  res.status(201).json({ hello: `${name}` });
+});
+
 module.exports = server;
